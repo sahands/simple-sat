0e36157a8421373df83c632cca28c34380396d3b
Sahand <sahands@gmail.com>
2014-04-19 13:38:36 -0700

    minor improvements to satinstance

--- satinstance.py
+++ satinstance.py
@@ -1,3 +1,17 @@
+"""
+Some notes on encoding:
+- Variables are encoded as numbers 0 to n - 1.
+- Literal v is encoded as 2 * v and ~v as 2 * v + 1. So the foremost
+  bit of a literal encodes whether it is negated or not. This can be
+  tested simply with checking if l & 1 is 0 or 1.
+- To negate a literal, we just have to toggle the foremost bit. This
+  can done easily by an XOR with 1: the negation of l is l ^ 1.
+- To get a literal's variable, we just need to shift to the right. This
+  can be done with l >> 1.
+
+Example: Let's say variable b is encoded with number 3. Then literal b
+is encoded as 2 * 3 = 6 and ~b as  2 * 3 + 1 = 7.
+"""
 from __future__ import division
 from __future__ import print_function
 
@@ -5,21 +19,7 @@ __author__ = 'Sahand Saba'
 
 
 class SATInstance(object):
-    def _parse_and_add_clause(self, line):
-        """
-        Some notes on encoding:
-        - Variables are encoded as numbers 0 to n - 1.
-        - Literal v is encoded as 2 * v and ~v as 2 * v + 1. So the foremost
-          bit of a literal encodes whether it is negated or not. This can be
-          tested simply with checking if l & 1 is 0 or 1.
-        - Similarly, to negate a literal, we just have to toggle the foremost
-          bit.  This can done easily by XORing with 1: the negation l is l ^ 1.
-        - To get a literal's variable, we just need to shift to the right. This
-          can be done with either l >> 1.
-
-        Example: Let's say variable b is encoded with number 3. Then literal b
-        is encoded as 2 * 3 = 6 and ~b as  2 * 3 + 1 = 7.
-        """
+    def parse_and_add_clause(self, line):
         clause = []
         for literal in line.split():
             negated = 1 if literal.startswith('~') else 0
@@ -31,16 +31,18 @@ class SATInstance(object):
             clause.append(encoded_literal)
         self.clauses.append(tuple(set(clause)))
 
+    def __init__(self):
+        self.variables = []
+        self.variable_table = dict()
+        self.clauses = []
+
     @classmethod
     def from_file(cls, file):
         instance = cls()
-        instance.variables = []
-        instance.variable_table = dict()
-        instance.clauses = []
         for line in file:
             line = line.strip()
             if len(line) > 0 and not line.startswith('#'):
-                instance._parse_and_add_clause(line)
+                instance.parse_and_add_clause(line)
         instance.n = len(instance.variables)
         return instance
 
